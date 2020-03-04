798d0514027ae9c6ece50f8b4782338cca080b67
gathumartins <gathumartins@gmail.com>
2020-03-04 19:15:49 +0300

    fourth commit

--- src/containers/SignIn.js
+++ src/containers/SignIn.js
@@ -49,8 +49,8 @@ class SignIn extends React.Component {
         <div className="app-login-main-content">
 
           <div className="app-logo-content d-flex align-items-center justify-content-center">
-            <Link className="logo-lg" to="/" title="Jambo">
-              <img src={require("assets/images/logo.png")} alt="jambo" title="jambo"/>
+            <Link className="logo-lg" to="/" title="synfin">
+              <img src={require("assets/images/logo.png")} alt="synfin" title="synfin"/>
             </Link>
           </div>
 
